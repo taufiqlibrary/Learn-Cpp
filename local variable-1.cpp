622da5d1ef679f670fe7424ce24cf5ee86047659
taufiqlibrary <taufiq.library@gmail.com>
2021-08-12 06:52:11 +0700

    Learn C++
    Learn C++

---
+++ local variable-1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+// Global variabel declaration:
+int g = 20;
+int main()
+{
+    // local variable declaration:
+    int g = 10;
+
+    cout <<g;
+
+    return 0;
+}
